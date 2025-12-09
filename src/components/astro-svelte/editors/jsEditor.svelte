252367acab79efaee30b1b9f2225e0f1359884bb
Kieran Wood <kieran@canadiancoding.ca>
2025-12-09 13:57:14 -0700

    Astro Svelte/editors: Added Simple Code Editor

---
+++ src/components/astro-svelte/editors/jsEditor.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { javascript } from "@codemirror/lang-javascript";
+import SimpleEditor from "@/components/astro-svelte/editors/simpleCodeEditor.ts.svelte"
+</script>
+
+<SimpleEditor
+  fileTitle="📄myfile.js"
+  initialData={`function helloWorld(){
+  console.log("Hello World!")
+}`}
+  language={javascript}
+/>
---
+++ src/components/astro-svelte/editors/simpleCodeEditor.ts.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { EditorView, basicSetup } from "codemirror";
+    import { type Extension } from "@codemirror/state";
+    import { html } from "@codemirror/lang-html";
+    import { gruvboxDark } from "@uiw/codemirror-theme-gruvbox-dark";
+    import {
+        abbreviationTracker,
+        EmmetKnownSyntax,
+    } from "@emmetio/codemirror6-plugin";
+
+    let view: EditorView;
+    let containerElement: HTMLElement;
+    let {
+        theme = gruvboxDark,
+        fileTitle = "",
+        language = html,
+        initialData = "<h1>Hello World</h1>",
+        emmetAbreviationsLanguage = "html",
+        contentStore = $bindable(undefined),
+    }: {
+        theme?: Extension;
+        fileTitle?: string;
+        initialData?: string;
+        emmetAbreviationsLanguage?: EmmetKnownSyntax | string;
+        language?: any;
+        contentStore?: undefined | string[];
+    } = $props();
+
+    onMount(() => {
+        // Create shadow root to isolate styles
+        const shadow = containerElement.attachShadow({ mode: "open" });
+        const wrapper = document.createElement("div");
+        wrapper.style.height = "100%";
+        shadow.appendChild(wrapper);
+
+        // Build Editor into wrapper
+        view = new EditorView({
+            parent: wrapper,
+            doc: initialData,
+            extensions: [
+                basicSetup,
+                language(),
+                theme,
+                abbreviationTracker({
+                    syntax: emmetAbreviationsLanguage as EmmetKnownSyntax,
+                }),
+                EditorView.updateListener.of((update) => {
+                    if (update.docChanged) {
+                        if (contentStore != undefined) {
+                            //   console.log("in, updated DOc", contentStore, view.state.doc.toString())
+                            if (contentStore.length == 0) {
+                                contentStore.push(view.state.doc.toString());
+                            } else {
+                                contentStore[0] = view.state.doc.toString();
+                            }
+                        }
+                    }
+                }),
+            ],
+        });
+        if (contentStore != undefined) {
+            //   console.log("in, updated DOc", contentStore, view.state.doc.toString())
+            if (contentStore.length == 0) {
+                contentStore.push(view.state.doc.toString());
+            } else {
+                contentStore[0] = view.state.doc.toString();
+            }
+        }
+    });
+</script>
+
+<div id="code-container">
+    {#if fileTitle}
+    <h2>{fileTitle}</h2>
+    {/if}
+    <div bind:this={containerElement}></div>
+</div>
+
+<style>
+    #code-container {
+        display: flex;
+        flex-direction: column;
+    }
+    h2 {
+        font-size: 1.3rem;
+    }
+</style>
